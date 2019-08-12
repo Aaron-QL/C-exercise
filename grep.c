35a42672a2d81ae7d680393bdf3f4015de81ef15
akaQin <qinlong@akaqin.me>
2019-08-12 15:57:14 +0800

    fahr

---
+++ grep.c
@@ -0,0 +1,5 @@
+#include <stdio.h>
+#define MAXLINE 1000
+
+int getline(char line[], int max)
+int search(char source[], char target[])
---
+++ test.c
@@ -0,0 +1,13 @@
+//
+// Created by akaQin on 2019-08-12.
+//
+#include <stdio.h>
+
+int main()
+{
+    char c;
+    while ((c = getchar()) != '')
+    {
+        putchar(c);
+    }
+}
