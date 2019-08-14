248b9131ada6c7fcca1919ba5b80c7ee649ca1e9
akaQin <qinlong@akaqin.me>
2019-08-14 14:39:50 +0800

    e

--- reverse.c
+++ reverse.c
@@ -1,5 +1,21 @@
 #include <stdio.h>
+#include "getLine.c"
+
+#define MAXLINE 1000
 
 int main() {
+    char line[MAXLINE];
 
+    int last = getLine(line, MAXLINE) - 1;
+
+
+    char target[last];
+
+    for (int i = 0; i < last; i++) {
+        target[i] = line[last - i - 1];
+    }
+    target[last] = '\0';
+
+    printf("%d\n", last);
+    printf("%s\n", target);
 }
