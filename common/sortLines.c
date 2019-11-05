735364c617673a8cbb2de7554971905aade9d497
akaQin <qinlong@akaqin.me>
2019-11-05 13:13:23 +0800

    writelines

--- common/sortLines.c
+++ common/sortLines.c
@@ -30,16 +30,24 @@ int readlines(char *lineptr, int maxlines)
 {
     int len, nlines;
     char *p, line[MAXLEN];
-    nlines = 0;
 
+    nlines = 0;
     while ((len = getline(line, MAXLEN)) > 0) {
         if (nlines >= maxlines || (p = alloc(len)) == NULL) {
             return -1;
         } else {
             line[len - 1] = '\0';
             strcpy(p, line);
-            lineptr[nlines++] = p
+            lineptr[nlines++] = p;
         }
     }
+
     return nlines;
+}
+
+void writelines(char *lineptr[], int nlines)
+{
+    while (nlines-- > 0) {
+        printf("%s\n", *lineptr++);
+    }
 }
