9c4604161979a2b8088dc748f6eeb4c86ade987f
akaQin <qinlong@akaqin.me>
2019-11-26 14:02:42 +0800

    bug fixed

--- common/sortLines-2.c
+++ common/sortLines-2.c
@@ -10,12 +10,13 @@
 #define MAXLEN 1000 //每个问本行最大长度
 
 char *lineptr[MAXLINES];
-int readlines(char *lineptr[], int maxlines);
+int reaadlines(char *lineptr[], int maxlines);
 void writelines(char *lineptr[], int maxlines);
 void my_qsort(char *lineptr[], int left, int right, int (*comp)(void *, void *));
 int my_numcmp(char *, char *);
 int my_strcmp(char *, char *);
 char *alloc(int);
+int my_getline(char *, int);
 
 int main(int argc, char *argv[])
 {
@@ -26,7 +27,7 @@ int main(int argc, char *argv[])
         numeric = 1;
     }
 
-    if ((nlines = readlines(lineptr, MAXLINES)) >= 0) {
+    if ((nlines = reaadlines(lineptr, MAXLINES)) >= 0) {
         my_qsort(
                 lineptr,
                 0,
@@ -42,15 +43,27 @@ int main(int argc, char *argv[])
 
 int reaadlines(char *lineptr[], int maxlines)
 {
-
+    int nlines = 0, len;
+    char line[MAXLEN], *p;
+    while ((len = my_getline(line, MAXLEN)) > 0) {
+        if (nlines > MAXLINES || (p = alloc(len)) == NULL) {
+            return -1;
+        }
+        line[len - 1] = '\0';
+        strcpy(p, line);
+        lineptr[nlines++] = p;
+    }
+    return nlines;
 }
 
 void writelines(char *lineptr[], int nlines)
 {
-
+    while (nlines-- > 0) {
+        printf("%s\n", *lineptr++);
+    }
 }
 
-int my_qsort(char *lineptr[], int left, int right, int (*comp)(void *, void *))
+void my_qsort(char *lineptr[], int left, int right, int (*comp)(void *, void *))
 {
 
 }
