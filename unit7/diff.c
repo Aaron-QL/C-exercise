0d9f90ff0090d683acf409b054c503b2c976eb2f
akaQin <qinlong@akaqin.me>
2019-12-08 19:28:40 +0800

    diff

--- unit7/diff.c
+++ unit7/diff.c
@@ -4,48 +4,52 @@
 
 #define MAXLINE 100
 
+void filecmp(FILE *, FILE *);
+
 int main(int argc, char *argv[])
 {
     if (argc != 3) {
         fprintf(stderr, "Usage: diff a.file b.file\n");
         exit(1);
     }
-    char *f1 = argv[1], *f2 = argv[2], *line1, *line2;
+
     FILE *fp1, *fp2;
 
-    if ((fp1 = fopen(f1, "r")) == NULL) {
-        fprintf(stderr, "open %s failed\n", f1);
+    if ((fp1 = fopen(*++argv, "r")) == NULL) {
+        fprintf(stderr, "open %s failed\n", *argv);
         exit(2);
-    }
-
-    if ((fp2 = fopen(f2, "r")) == NULL) {
-        fprintf(stderr, "open %s failed\n", f2);
+    } else if ((fp2 = fopen(*++argv, "r")) == NULL) {
+        fprintf(stderr, "open %s failed\n", *argv);
         exit(2);
+    } else {
+        filecmp(fp1, fp2);
+        fclose(fp1);
+        fclose(fp2);
+        exit(0);
     }
+}
 
-    while ((fgets(line1, MAXLINE, fp1) != NULL)
-            && (fgets(line2, MAXLINE, fp2) != NULL))
-    {
-        if (strcmp(line1, line2) != 0) {
-            printf("%s : %s\n", f1, line1);
-            printf("%s : %s\n", f2, line2);
+void filecmp(FILE *fp1, FILE *fp2)
+{
+    char line1[MAXLINE], line2[MAXLINE];
+    char *lp1, *lp2;
+
+    do {
+        lp1 = fgets(line1, MAXLINE, fp1);
+        lp2 = fgets(line2, MAXLINE, fp2);
+        if (lp1 == line1 && lp2 == line2) {
+            if (strcmp(line1, line2) != 0) {
+                printf("difference:\n");
+                printf("%s", line1);
+                printf("%s", line2);
+            }
+        } else if (lp1 != line1 && lp2 == line2) {
+            printf("file1 to end\n");
+            break;
+        } else if (lp1 == line1 && lp2 != line2) {
+            printf("file2 to end\n");
+            break;
         }
-    }
+    } while (lp1 == line1 && lp2 == line2);
 
-    while (fgets(line1, MAXLINE, fp1) != NULL) {
-        printf("%s : %s\n", f1, line1);
-    }
-
-    while (fgets(line2, MAXLINE, fp2) != NULL) {
-        printf("%s : %s\n", f2, line2);
-    }
-
-    fclose(fp1);
-    fclose(fp2);
-    if (ferror(stdout)) {
-        fprintf(stderr, "error writing stdout\n");
-        exit(3);
-    }
-
-    exit(0);
 }
