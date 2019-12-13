d23613039127c47beeedeb190087c2665f8a7446
akaQin <qinlong@akaqin.me>
2019-12-13 13:54:39 +0800

    getchar

--- common/my_getchar.c
+++ common/my_getchar.c
@@ -3,12 +3,13 @@
 int getchar(void)
 {
     static char buf[BUFSIZE];
-    static char *bufp = &buf;
+    static char *bufp = buf;
     static int n = 0;
 
     if (n == 0) {
-        n = read(0, buf, sizeof(buf));
+        n = read(0, buf, sizeof buf);
         bufp = buf;
     }
-    return (--n >= 0) ? (unsigned char) *bufp : EOF;
+
+    return (--n >= 0) ? (unsigned char) *bufp++ : EOF;
 }
